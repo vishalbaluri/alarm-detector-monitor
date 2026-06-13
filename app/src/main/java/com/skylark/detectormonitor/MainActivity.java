361903927064f266326bc1102c9717a7980d3d7e
Vishal Baluri <vishalbaluri3004@gmail.com>
2026-06-13 12:19:31 +0530

    Updated imports in main activity.java

--- app/src/main/java/com/skylark/detectormonitor/MainActivity.java
+++ app/src/main/java/com/skylark/detectormonitor/MainActivity.java
@@ -56,7 +56,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.TimeUnit;
-import okhttp3.*;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
